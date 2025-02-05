82135538930fe60d41482c72315b3373d4e3bde9
BirchCompute <none>
2025-02-05 01:20:26 -0700

    Refactored and made the second chart fit

--- typeBarGraph.py
+++ typeBarGraph.py
@@ -1,44 +1,60 @@
 
 import matplotlib.pyplot as plt
 import pandas as pd
+
+
 #Read the data into pandas dataFrames
 dfPokeData = pd.read_csv("pokemonInfo.csv")
 dfTypeColors  = pd.read_csv("typeColors.csv")
+
+
 #Count how many times a type shows up in Pokedex
-mainTypeCount = dfPokeData["Type 1"].value_counts()
-totalTypeCount = mainTypeCount.to_frame()
-secondTypeCount = dfPokeData["Type 2"].value_counts()
-totalTypeCount["count2"] = secondTypeCount
-print(totalTypeCount)
+totalTypeCount = dfPokeData["Type 1"].value_counts().to_frame()
+totalTypeCount["count2"] = dfPokeData["Type 2"].value_counts()
+totalTypeCount["total"] = totalTypeCount["count"] + totalTypeCount["count2"]
+
 #Make the color list
 typeColors = []
-for c in dfTypeColors.itertuples():
-  if("GEN 7 — SM" in c.Gen and "inium" not in c.Gen):
-    typeColors.append(c.colors[:7])
+typeColorsDark = []
+for color in dfTypeColors.itertuples():
+  if("GEN 7 — SM" in color.Gen and "inium" not in color.Gen):
+    typeColors.append(color.colors[:7])
+    #Turns the hex color into a darker rgb tuple
+    typeColorsDark.append(tuple((int(color.colors[:7][i+1:i+3], 16)/500)  for i in (0, 2, 4)))
 
-    
 #Add colors to the dataFrame
+#The color list is alphabetical, so sort the totals dataframe to be alphabetical by type
 totalTypeCount = totalTypeCount.sort_values(by= "Type 1")
-totalTypeCount["typeColors"] = type
-
-Colorsdef hex_to_rgb(hexa):
-    hexa = hexa[1:]
-    return tuple((int(hexa[i:i+2], 16)/500)  for i in (0, 2, 4))
-
-totalTypeCount["total"] = totalTypeCount["count"] + totalTypeCount["count2"]
-#totalTypeCount = totalTypeCount.sort_values(by= "total")
-totalTypeCount["lightTypeColors"]  = totalTypeCount["typeColors"]
-totalTypeCount["lightTypeColors"] = totalTypeCount["lightTypeColors"].apply(hex_to_rgb)
-print(totalTypeCount)
-
-for x in totalTypeCount.itertuples():
-    print(x.Index, x.total, end = ", ")fig, axs = plt.subplots()
-
-axs.bar(totalTypeCount.index, totalTypeCount["count"], color = totalTypeCount["typeColors"], width = 1, edgecolor='black', linewidth=2)
-axs.bar(totalTypeCount.index, totalTypeCount["count2"], color = totalTypeCount["lightTypeColors"], bottom = totalTypeCount["count"], width = 1, edgecolor='black', linewidth=2)
+totalTypeCount["typeColors"] = typeColors
+totalTypeCount["darkTypeColors"] = typeColorsDark
 
 
-axs.tick_params(axis='x', labelrotation=75, length = 2)
-axs.set_title("Pokemon primary and secondary\n type prevalence as of 1025 Pokemon (alphabetical)")
-axs.set_xlabel("Types")
-axs.set_ylabel("Count")
+#Force a size for the figure
+plt.rcParams['figure.figsize'] = [16,6]
+
+#Prepare the figure
+fig, (axs1, axs2) = plt.subplots(1,2)
+
+
+#Make the actual bars for first chart
+axs1.bar(totalTypeCount.index, totalTypeCount["count"], color = totalTypeCount["typeColors"], width = 1, edgecolor='black', linewidth=2)
+axs1.bar(totalTypeCount.index, totalTypeCount["count2"], color = totalTypeCount["darkTypeColors"], bottom = totalTypeCount["count"], width = 1, edgecolor='black', linewidth=2)
+
+#Mark first axes
+axs1.tick_params(axis='x', labelrotation=75, length = 2)
+axs1.set_title("Pokemon primary and secondary\n type prevalence as of 1025 Pokemon (alphabetical)")
+axs1.set_xlabel("Types")
+axs1.set_ylabel("Count")
+
+#Sort by total and do it again for the second chart
+totalTypeCount = totalTypeCount.sort_values(by= "total")
+
+#Make the actual bars for second chart
+axs2.bar(totalTypeCount.index, totalTypeCount["count"], color = totalTypeCount["typeColors"], width = 1, edgecolor='black', linewidth=2)
+axs2.bar(totalTypeCount.index, totalTypeCount["count2"], color = totalTypeCount["darkTypeColors"], bottom = totalTypeCount["count"], width = 1, edgecolor='black', linewidth=2)
+
+#Mark the second axes
+axs2.tick_params(axis='x', labelrotation=75, length = 2)
+axs2.set_title("Pokemon primary and secondary\n type prevalence as of 1025 Pokemon (sorted)")
+axs2.set_xlabel("Types")
+axs2.set_ylabel("Count")
